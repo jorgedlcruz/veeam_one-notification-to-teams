40b2cc1c0b003a8f3ac80766fc7842fab9c62c16
Jorge de la Cruz <jorgedlcruz@gmail.com>
2019-11-19 21:12:40 +0000

    v0.1 VONE 2 TEAMS
    v0.1 VONE 2 TEAMS

--- VeeamONE-Notification.ps1
+++ VeeamONE-Notification.ps1
@@ -17,7 +17,7 @@ param([string]$AlarmName,[string]$NodeName,[string]$Summary,[string]$Time,[strin
     
  #>
 
-$uri = "https://outlook.office.com/webhook/714f2dd0-2c93-49da-b9bb-6223c2bbe5e0@a51ea1e1-9b68-496e-8953-95ef2a1a38f5/IncomingWebhook/832fd5d6fd4540359f12b2f584f32279/291b10fd-bb83-4e51-9365-302f403234a8"
+$uri = "YOUR-MICROSOFT-TEAMS-WEBHOOK"
 
 $body = ConvertTo-JSON @{
     text = "[$($ID)] Alarm - $($AlarmName) for $($NodeName) has been changed to $($Status) (previous state: $($OldStatus))"
